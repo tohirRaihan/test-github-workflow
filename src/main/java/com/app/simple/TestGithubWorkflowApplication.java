c6a6db64fd267de489ce7b1ddcbaf2b03685f5cf
Tohir Raihan <tohir.raihan@gmail.com>
2025-02-15 22:10:45 +0600

    first commit

---
+++ src/main/java/com/app/simple/TestGithubWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.app.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGithubWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGithubWorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/simple/TestGithubWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGithubWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

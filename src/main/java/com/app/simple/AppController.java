483855588cc6f83aeb959d49ecadc4524d6271e3
Tohir Raihan <tohir.raihan@gmail.com>
2025-02-15 22:11:06 +0600

    first commit

--- src/main/java/com/app/simple/AppController.java
+++ src/main/java/com/app/simple/AppController.java
@@ -1,4 +1,12 @@
 package com.app.simple;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AppController {
+  @GetMapping
+  public String helloWorld() {
+    return "Hello World from Testing Application!";
+  }
 }
